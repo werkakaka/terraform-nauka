fc4795a5efa08212ab717f91a90e32ffbbe0205b
Weronika <kasprzakweronika11@gmail.com>
2026-07-13 13:50:57 +0200

    Lekcja 2: Docker provider - obraz i kontener nginx

---
+++ lekcja2/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "moj_nginx" {
+  name  = "moj-nginx-terraform"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
